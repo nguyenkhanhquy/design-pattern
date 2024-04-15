658855cc2a6d9e70f9656e548f2cb2d1156ec1c4
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-04-15 09:20:35 +0700

    modified: Strategy

--- Behavioral/Strategy/21110282_Strategy/src/com/design/pattern/strategy/Main.java
+++ Behavioral/Strategy/21110282_Strategy/src/com/design/pattern/strategy/Main.java
@@ -8,17 +8,17 @@ public class Main {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         // Thêm sản phẩm vào giỏ hàng
-        Item item1 = new Item("123", 50);
-        Item item2 = new Item("456", 30);
+        Item item1 = new Item("123", 150);
+        Item item2 = new Item("456", 80);
         shoppingCart.addItem(item1);
         shoppingCart.addItem(item2);
 
         // Thanh toán bằng Credit Card
-        PaymentStrategy creditCardStrategy = new CreditCardStrategy("John Doe", "1234567890123456", "123", "12/25");
+        PaymentStrategy creditCardStrategy = new CreditCardStrategy("Khanh Quy", "1234567890123456", "123", "12/25");
         shoppingCart.checkout(creditCardStrategy);
 
         // Thanh toán qua PayPal
-        PaymentStrategy paypalStrategy = new PaypalStrategy("john.doe@example.com", "mypassword");
+        PaymentStrategy paypalStrategy = new PaypalStrategy("khanhquy@email.com", "mypassword123");
         shoppingCart.checkout(paypalStrategy);
 
         // Thanh toán qua Bitcoin
